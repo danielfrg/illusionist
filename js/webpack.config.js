00f8d72eb04f993fb3f6f6b6665d07e3e03f4887
Daniel Rodriguez <daniel@danielfrg.com>
2020-10-04 18:21:12 -0400

    Fix npm package release stuff

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -1,60 +1,8 @@
 var path = require("path");
+const FileManagerPlugin = require("filemanager-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-var rules = [
-    {
-        test: /\.s?[ac]ss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-    },
-    {
-        test: /\.(js)$/,
-        exclude: /node_modules/,
-        use: ["babel-loader"],
-    },
-    // required to load font-awesome
-    {
-        test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
-        use: {
-            loader: "url-loader",
-            options: {
-                limit: 10000,
-                mimetype: "application/font-woff",
-            },
-        },
-    },
-    {
-        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
-        use: {
-            loader: "url-loader",
-            options: {
-                limit: 10000,
-                mimetype: "application/font-woff",
-            },
-        },
-    },
-    {
-        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        use: {
-            loader: "url-loader",
-            options: {
-                limit: 10000,
-                mimetype: "application/octet-stream",
-            },
-        },
-    },
-    { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, use: "file-loader" },
-    {
-        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        use: {
-            loader: "url-loader",
-            options: {
-                limit: 10000,
-                mimetype: "image/svg+xml",
-            },
-        },
-    },
-];
-
-var distRoot = path.resolve(
+var pythonPkgStatic = path.resolve(
     __dirname,
     "..",
     "python",
@@ -69,23 +17,83 @@ var distRoot = path.resolve(
 
 module.exports = [
     {
-        entry: [path.resolve(__dirname, "src", "index.js")],
-        output: {
-            filename: "illusionist.js",
-            path: distRoot,
-        },
-        module: { rules: rules },
-        mode: "development",
-        devtool: "source-map",
-    },
-    {
         entry: [path.resolve(__dirname, "src", "embed.js")],
         output: {
+            path: path.resolve(__dirname, "dist"),
             filename: "illusionist-embed.js",
-            path: distRoot,
         },
-        module: { rules: rules },
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: "illusionist.css",
+            }),
+            // Copy the output to the Python Package
+            new FileManagerPlugin({
+                onEnd: {
+                    copy: [
+                        {
+                            source: "./dist/*.*",
+                            destination: pythonPkgStatic,
+                        },
+                    ],
+                },
+            }),
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.(js)$/,
+                    include: path.resolve(__dirname, "src"),
+                    // exclude: /node_modules/,
+                    use: ["babel-loader"],
+                },
+                // Jupyter Widgets CSS
+                {
+                    test: /\.css$/i,
+                    use: [MiniCssExtractPlugin.loader, "css-loader"],
+                    // use: ["null-loader"],
+                },
+                // Jupyter Widget Icons
+                { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, use: ["url-loader"] },
+                // Required to load font-awesome
+                { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, use: ["file-loader"] },
+                { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, use: ["file-loader"] },
+                { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, use: ["file-loader"] },
+                { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, use: ["file-loader"] },
+            ],
+        },
         mode: "development",
         devtool: "source-map",
     },
+    // {
+    //     entry: [path.resolve(__dirname, "src", "index.js")],
+    //     output: {
+    //         path: path.resolve(__dirname, "lib"),
+    //         filename: "index.js",
+    //         libraryTarget: "commonjs2",
+    //     },
+    //     module: {
+    //         rules: [
+    //             {
+    //                 test: /\.(js)$/,
+    //                 include: path.resolve(__dirname, "src"),
+    //                 // exclude: /node_modules/,
+    //                 use: ["babel-loader"],
+    //             },
+    //             // Jupyter Widgets CSS
+    //             { test: /\.css$/i, use: ["url-loader"] },
+    //             // Jupyter Widget Icons
+    //             { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, use: ["url-loader"] },
+    //             // Required to load font-awesome
+    //             { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, use: ["file-loader"] },
+    //             { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, use: ["file-loader"] },
+    //             { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, use: ["file-loader"] },
+    //             { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, use: ["file-loader"] },
+    //         ],
+    //     },
+    //     optimization: {
+    //         minimize: false,
+    //     },
+    //     mode: "development",
+    //     devtool: "source-map",
+    // },
 ];
